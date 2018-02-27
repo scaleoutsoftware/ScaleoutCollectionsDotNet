2c0314573493df8e5aafbfb0ccd5d7e0c782ac88
markwaterman <markwaterman@users.noreply.github.com>
2018-02-26 21:56:46 -0800

    Basic indexer support.

--- src/Scaleout.Collections/RouletteDictionary.cs
+++ src/Scaleout.Collections/RouletteDictionary.cs
@@ -1,21 +1,196 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Scaleout.Collections
 {
     public class RouletteDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
-        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private int _count = 0;
+        IEqualityComparer<TKey> _comparer;
+
+        private struct Bucket
+        {
+            public int HashCode;
+            public TKey Key;
+            public TValue Value;
+            public bool isOccupied;
+            public bool isTombstone;
+        }
+
+        Bucket[] _buckets;
+
+        public int Count => _count;
+
+        public bool IsReadOnly => false;
+
+        public RouletteDictionary(int capacity = 1, IEqualityComparer<TKey> comparer = null)
+        {
+            _comparer = comparer ?? EqualityComparer<TKey>.Default;
+
+            // we keep the load factor below .7, so add some wiggle room to the requested
+            // capacity to prevent resize operations
+            int actualCapacity = Primes.Next((int)(capacity * 1.5));
+            _buckets = new Bucket[actualCapacity];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private float LoadFactor() { return (float)_count / _buckets.Length; }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int hashcode = _comparer.GetHashCode(key) & 0x7FFFFFFF;
+                int bucketIndex = hashcode % _buckets.Length;
+                int probeIndex = bucketIndex;
+                int probeCount = 0;
+
+                while (probeCount <= _buckets.Length)
+                {
+                    if (_buckets[probeIndex].isTombstone)
+                    {
+                        // need to probe again.
+                        probeCount++;
+                        probeIndex = bucketIndex + (probeCount * probeCount);
+                        if (probeIndex >= _buckets.Length)
+                            probeIndex = probeIndex % _buckets.Length;
+                        continue;
+                    }
+
+                    if (!_buckets[probeIndex].isOccupied)
+                    {
+                        // didn't find the entry
+                        throw new KeyNotFoundException("key does not exist in the collection");
+                    }
+
+                    if (hashcode == _buckets[probeIndex].HashCode && _comparer.Equals(key, _buckets[probeIndex].Key))
+                    {
+                        // found the entry, return the value
+                        return _buckets[probeIndex].Value;
+                    }
+                    else
+                    {
+                        // collision, probe again.
+                        probeCount++;
+                        probeIndex = bucketIndex + (probeCount * probeCount);
+                        if (probeIndex >= _buckets.Length)
+                            probeIndex = probeIndex % _buckets.Length;
+                        continue;
+                    }
+                }
+                throw new ApplicationException("too many probes");
+            }
+
+            set
+            {
+                int hashcode = _comparer.GetHashCode(key) & 0x7FFFFFFF;
+                int bucketIndex = hashcode % _buckets.Length;
+                int probeIndex = bucketIndex;
+
+                int indexOfFirstTombstone = -1;
+                int probeCount = 0;
+
+                while (probeCount <= _buckets.Length)
+                {
+                    if (_buckets[probeIndex].isTombstone)
+                    {
+                        if (indexOfFirstTombstone == -1)
+                            indexOfFirstTombstone = probeIndex;
+
+                        // need to probe again.
+                        probeCount++;
+                        probeIndex = bucketIndex + (probeCount * probeCount);
+                        if (probeIndex >= _buckets.Length)
+                            probeIndex = probeIndex % _buckets.Length;
+                        continue;
+                    }
+
+                    if (!_buckets[probeIndex].isOccupied)
+                    {
+                        // didn't find the entry, adding new value.
+                        _count++;
+
+                        // fill in a tombstone, if possible:
+                        if (indexOfFirstTombstone != -1)
+                            probeIndex = indexOfFirstTombstone;
+
+                        _buckets[probeIndex].HashCode = hashcode;
+                        _buckets[probeIndex].Key = key;
+                        _buckets[probeIndex].Value = value;
+                        _buckets[probeIndex].isTombstone = false;
+                        _buckets[probeIndex].isOccupied = true;
+
+                        if (LoadFactor() > 0.7)
+                            Resize();
+                        return;
+                    }
+
+                    if (hashcode == _buckets[probeIndex].HashCode && _comparer.Equals(key, _buckets[probeIndex].Key))
+                    {
+                        // found the entry, set new value
+                        _buckets[probeIndex].Value = value;
+                        return;
+                    }
+                    else
+                    {
+                        // collision, probe again.
+                        probeCount++;
+                        probeIndex = bucketIndex + (probeCount * probeCount);
+                        if (probeIndex >= _buckets.Length)
+                            probeIndex = probeIndex % _buckets.Length;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private void Resize()
+        {
+            var newBuckets = new Bucket[Primes.Next((_buckets.Length * 2) + 1)];
+
+            for (int i = 0; i < _buckets.Length; i++)
+            {
+                if (!_buckets[i].isOccupied || _buckets[i].isTombstone)
+                    continue;
+
+                int bucketIndex = _buckets[i].HashCode % newBuckets.Length;
+                int probeIndex = bucketIndex;
+                int probeCount = 0;
+                while (true)
+                {
+                    if (probeCount == newBuckets.Length)
+                        throw new ApplicationException("too many probes");
+
+                    if (!newBuckets[probeIndex].isOccupied)
+                    {
+                        newBuckets[probeIndex].HashCode = _buckets[i].HashCode;
+                        newBuckets[probeIndex].Key = _buckets[i].Key;
+                        newBuckets[probeIndex].Value = _buckets[i].Value;
+                        newBuckets[probeIndex].isTombstone = false;
+                        newBuckets[probeIndex].isOccupied = true;
+                        break;
+                    }
+                    else
+                    {
+                        // collision. probe again.
+                        probeCount++;
+                        probeIndex = bucketIndex + (probeCount * probeCount);
+                        if (probeIndex >= newBuckets.Length)
+                            probeIndex = probeIndex % newBuckets.Length;
+                    }
+
+                }
+            }
+
+            _buckets = newBuckets;
+        }
 
         public ICollection<TKey> Keys => throw new NotImplementedException();
 
         public ICollection<TValue> Values => throw new NotImplementedException();
-
-        public int Count => throw new NotImplementedException();
-
-        public bool IsReadOnly => throw new NotImplementedException();
 
         public void Add(TKey key, TValue value)
         {
---
+++ src/UnitTests/RouletteTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Scaleout.Collections;
+
+namespace UnitTests
+{
+    public class RouletteTests
+    {
+        [Fact]
+        public void EmptyDict()
+        {
+            var rd = new RouletteDictionary<int, int>();
+            Assert.Throws<KeyNotFoundException>(() => rd[42]);
+        }
+
+        [Fact]
+        public void OneItem()
+        {
+            var rd = new RouletteDictionary<string, string>(1);
+            rd["hello"] = "world";
+            Assert.Equal("world", rd["hello"]);
+            Assert.Throws<KeyNotFoundException>(() => rd["foo"]);
+        }
+
+        [Fact]
+        public void OneItemBigCapacity()
+        {
+            var rd = new RouletteDictionary<string, string>(100_000);
+            rd["hello"] = "world";
+            Assert.Equal("world", rd["hello"]);
+            Assert.Throws<KeyNotFoundException>(() => rd["foo"]);
+        }
+
+        [Fact]
+        public void CustomComparer()
+        {
+            var rd = new RouletteDictionary<string, string>(comparer: StringComparer.OrdinalIgnoreCase);
+            rd["hello"] = "world";
+            Assert.Equal("world", rd["HELLO"]);
+            Assert.Throws<KeyNotFoundException>(() => rd["foo"]);
+        }
+
+        [Fact]
+        public void TenThousand()
+        {
+            // start with low capacity to exercise resizing.
+            var rd = new RouletteDictionary<string, int>(1);
+
+            for (int i = 0; i < 10_000; i++)
+            {
+                rd[i.ToString()] = i;
+            }
+
+            Assert.Equal(10_000, rd.Count);
+
+            for (int i = 0; i < 10_000; i++)
+            {
+                Assert.Equal(i, rd[i.ToString()]);
+            }
+        }
+    }
+}
